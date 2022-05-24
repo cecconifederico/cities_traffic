7eda33e57c91eb8e15da2485d5496f7965cce259
Federico Cecconi <federico.cecconi@qbt.ch>
2022-05-24 11:37:09 +0200

    update codice simulazione

--- city_car_sim.py
+++ city_car_sim.py
@@ -6,15 +6,17 @@ Created on Thu May 19 09:55:08 2022
 """
 import pandas as pd
 import numpy as np
+import random
 
 class CityCar():
-    car_id=""
-    gasoline = 0
-    visited = []
+    # car_id=""
+    # gasoline = 0
+    # visited = []
     
     def __init__(self,gasoline,car_id):
         self.car_id=str(car_id)+"id"
         self.gasoline = gasoline
+        self.visited = []
         
     def __str__(self):
         # quando mostro una city car scrivo
@@ -27,6 +29,38 @@ def init_simulation(n):
     for i in range(n):
         # crea n car con un valore per la benzina che va da 100 a 1000
         cars_list.append(CityCar(np.random.randint(100,1000),i))
+
+def calcola_arrivo(percorsi,start):
+    appoggio = []
+    for percorso in percorsi:
+        if percorso[0]==start:
+            appoggio.append(percorso)
+    if len(appoggio)==0:
+        return(['none',-1])
+    else:
+        x=random.choice(appoggio)
+        return([x[1],x[2]])        
+
+
+
         
 init_simulation(100)        
-        
+df=pd.read_csv('citta.csv',delimiter=',')  
+df_list=df.values.tolist()
+
+t = 0
+durata = 1
+
+l = df['partenza'].tolist()
+m = list(set(l))
+
+for j in cars_list:
+    x=random.choice(m)
+    j.visited.append(x)
+    
+while t < durata:
+    random.shuffle(cars_list)
+    for a in cars_list:
+        print(calcola_arrivo(df_list,a.visited[0]))
+    t+=1    
+      
